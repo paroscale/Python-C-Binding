a4c71ed09fe7826613bd5cbc5ce84b1f2467c96b
Ashwini Patil <ashwinip309@gmail.com>
2021-04-05 10:10:42 +0530

    Basic Calculator code using CFFI (writing functions in C  and calling that in Python)

---
+++ build_calc.py
@@ -0,0 +1,19 @@
+import os
+import cffi 
+if __name__ == "__main__":
+    ffi = cffi.FFI()    
+    with open(os.path.join(os.path.dirname(__file__), "Calc.h")) as f:
+        ffi.cdef(f.read())    
+        ffi.set_source("myCalc",
+        # Since we are calling a fully built library directly no custom source
+        # is necessary. We need to include the .h files, though, because behind
+        # the scenes cffi generates a .c file which contains a Python-friendly
+        # wrapper around each of the functions.
+        '#include "Calc.h"',
+        # The important thing is to include the pre-built lib in the list of
+        # libraries we are linking against:
+        sources = ['Calc.c'],
+        libraries=["Calc"],
+        library_dirs=[os.path.dirname(__file__),],
+    )    
+    ffi.compile()
---
+++ test.py
@@ -0,0 +1,6 @@
+from myCalc.lib import *
+
+print("Addition is : ", addition(2,3))
+print("Subtraction is : " , subtraction(8,4))
+print("Multiplication is : ", multiplication(2,3))
+print("Division is : ", division(12,3))
